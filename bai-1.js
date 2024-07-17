34cbf9c396524f5f4ef390fc79f374285b4b9458
Quốc Nguyễn <nguyenminhquoc812005@gmail.com>
2024-07-17 15:53:51 +0700

    nop bai tap

---
+++ bai-1.js
@@ -0,0 +1,10 @@
+function tinhToan() {
+    let a = +document.getElementById("a").value
+    let b = +document.getElementById("b").value
+    if (a % b === 0) {
+        document.getElementById("result").innerHTML = a + " chia hết cho " + b;
+    } else {
+        document.getElementById("result").innerHTML = a + " không chia hết cho " + b;
+    }
+}
+
